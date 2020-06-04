9cee061a82dd28a32e2e6b165e79bd242472195e
snatu1996 <shubham.natu@cognizant.com>
2020-06-04 15:01:26 +0530

    Implementing findById and findAll with spring-data-jpa

--- src/main/java/com/shubham/spring/JPA/spring/JPA/demo/UserRepositoryCommandLineRunner.java
+++ src/main/java/com/shubham/spring/JPA/spring/JPA/demo/UserRepositoryCommandLineRunner.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component  //Since it will be managed by Spring Boot
 public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 
@@ -20,6 +23,17 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User user = new User("Jill","Admin");
         userRepository.save(user);  //Making the user entity persist through the DaoService created and it stores the user in the database
+
+        //save() can be used for insert and update both
+
+        Optional<User> userWithId = userRepository.findById(1L);
+
+        logger.info("User with id"+userWithId);
+
+        List<User> users = userRepository.findAll();
+
+        logger.info("Users list"+users);
+
         logger.info("User Saved"+user);
     }
 }
